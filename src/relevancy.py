c8859e2d39c4a69c905d8a10a9465ea469967ad3
Brieuc lehmann <b.lehmann@ucl.ac.uk>
2024-08-19 11:25:00 +0100

    Update relevancy.py

--- src/relevancy.py
+++ src/relevancy.py
@@ -148,17 +148,12 @@ def run_all_day_paper(
     if date is None:
         date = datetime.today().strftime('%a, %d %b %y')
         # string format such as Wed, 10 May 23
-    print ("the date for the arxiv data is: ", date)
+    print ("the date for the biorxiv data is: ", date)
 
     all_papers = [json.loads(l) for l in open(f"{data_dir}/{date}.jsonl", "r")]
     print (f"We found {len(all_papers)}.")
 
-    all_papers_in_subjects = [
-        t for t in all_papers
-        if bool(set(process_subject_fields(t['subjects'])) & set(query['subjects']))
-    ]
-    print(f"After filtering subjects, we have {len(all_papers_in_subjects)} papers left.")
-    ans_data = generate_relevance_score(all_papers_in_subjects, query, model_name, threshold_score, num_paper_in_prompt, temperature, top_p)
+    ans_data = generate_relevance_score(all_papers, query, model_name, threshold_score, num_paper_in_prompt, temperature, top_p)
     utils.write_ans_to_file(ans_data, date, output_dir="../outputs")
     return ans_data
 
